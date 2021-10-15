867f21b237338203223464c32a37dc009d139bee
Javier <marandajavier@gmail.com>
2021-10-15 19:44:25 -0300

    actualizando cv

--- enviar.php
+++ enviar.php
@@ -1,16 +1,17 @@
 <?php 
-$myemail = 'marandajavier@gmail.com';
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
 
-$to = $myemail;
-$email_subject = "Nuevo mensaje: $subject";
-$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
-$headers = "From: $email";
-
-mail($to, $email_subject, $email_body, $headers);
-echo "El mensaje se ha enviado correctamente";
-?>
-
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $header = "From: noreply@example.com" . "\r\n";
+        $header. = "Reply-To: noreply@example.com" . "\r\n";
+        $header. = "X-Mailer: PHP/". phpversion();
+        $mail = mail($email,$subject,$message,$header);
+        if ($mail) {
+            echo "<h4>¡Mail enviado exitosamente!";
+        }
+    }
+}
